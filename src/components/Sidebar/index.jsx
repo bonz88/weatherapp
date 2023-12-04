aef500608b76c18ee3a17ed9ed2922593b14d335
Kenan <kenan@Kenans-MacBook-Air.local>
2023-12-04 13:37:38 +0100

    added currentCityContext

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,27 +1,27 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useContext } from "react";
 import axios from "axios";
 import Logo from "../Logo";
 import SidebarCurrentLocation from "../SidebarCurrentLocation";
+import { CurrentCityContext } from "../../contexts/currentCityContext";
 //import ListCities from "../ListCities";
 //import InputCities from "../InputCities";
 //import Unit from "../Unit";
 
-const Sidebar = (props) => {
-    const [cities, setCities] = useState([]);
-    const [error, setError] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
+const Sidebar = () => {
+  const [cities, setCities] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
+  const { currentCity } = useContext(CurrentCityContext);
 
-    return (
-        <>
-        <nav className="px-8">
-            <Logo />
-            <SidebarCurrentLocation currentCity={props.currentCity} />
-            
-        </nav>
-        </>
-    )
-}
+  return (
+    <>
+      <nav className="px-8">
+        <Logo />
+        <SidebarCurrentLocation currentCity={currentCity} />
+      </nav>
+    </>
+  );
+};
 
 export default Sidebar;
